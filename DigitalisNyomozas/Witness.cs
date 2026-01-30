a8dd8f8845a9cbba97b0f9472940be72c56b7a46
Vince <molnar.vince.bela@petrik.hu>
2026-01-30 09:24:13 +0100

    Kesz Witness osztaly

---
+++ DigitalisNyomozas/Witness.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozas
+{
+	internal class Witness
+	{
+		private Person person;
+		private string vallomasSzoveg;
+		private int vallomasDatum;
+
+		public Witness(Person person, string vallomasSzoveg, int vallomasDatum)
+		{
+			this.person = person;
+			this.vallomasSzoveg = vallomasSzoveg;
+			this.vallomasDatum = vallomasDatum;
+		}
+
+		public string VallomasSzoveg { get => vallomasSzoveg; set => vallomasSzoveg = value; }
+		public int VallomasDatum { get => vallomasDatum; set => vallomasDatum = value; }
+		internal Person Person { get => person; set => person = value; }
+	}
+}
