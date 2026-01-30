86962c203f1966a1a8492f16ac0a099d6b5a735b
Vince <molnar.vince.bela@petrik.hu>
2026-01-30 09:27:13 +0100

    Kesz TimeLineEvent

---
+++ DigitalisNyomozas/TimeLineEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozas
+{
+	internal class TimeLineEvent
+	{
+		private string date;
+		private string esemenyLeiras;
+
+		public TimeLineEvent(string date, string esemenyLeiras)
+		{
+			this.date = date;
+			this.esemenyLeiras = esemenyLeiras;
+		}
+
+		public string Date { get => date; set => date = value; }
+		public string EsemenyLeiras { get => esemenyLeiras; set => esemenyLeiras = value; }
+	}
+}
