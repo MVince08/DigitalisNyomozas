4f0f8f83c6ad1d01ac3fce290d6e7963fb50f9c5
Vince <molnar.vince.bela@petrik.hu>
2026-01-30 08:59:33 +0100

    Kesz Felhasznalo osztaly

---
+++ DigitalisNyomozas/Felhasznalo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozas
+{
+	internal class Felhasznalo
+	{
+		private string name;
+		private string azonosito;
+		private string description;
+
+		public Felhasznalo(string name, string azonosito, string description)
+		{
+			this.name = name;
+			this.azonosito = azonosito;
+			this.description = description;
+		}
+
+		public string Name { get => name; set => name = value; }
+		public string Azonosito { get => azonosito; set => azonosito = value; }
+		public string Description { get => description; set => description = value; }
+	}
+}
