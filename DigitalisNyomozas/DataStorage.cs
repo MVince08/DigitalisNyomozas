3c9e77ffb91593ce9218594328d55a6812394da2
Vince <molnar.vince.bela@petrik.hu>
2026-01-30 09:51:06 +0100

    Kesz DataStorage osztaly

---
+++ DigitalisNyomozas/DataStorage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozas
+{
+	internal class DataStorage
+	{
+		private List<Felhasznalo> felhasznalo;
+		private List<Case> ugyek;
+		private List<Person> szemelyek;
+		private List<Evidence> bizonyitekok;
+
+		public DataStorage(List<Felhasznalo> felhasznalo, List<Case> ugyek, List<Person> szemelyek, List<Evidence> bizonyitekok)
+		{
+			this.felhasznalo = felhasznalo;
+			this.ugyek = ugyek;
+			this.szemelyek = szemelyek;
+			this.bizonyitekok = bizonyitekok;
+		}
+
+		internal List<Felhasznalo> Felhasznalo { get => felhasznalo; set => felhasznalo = value; }
+		internal List<Case> Ugyek { get => ugyek; set => ugyek = value; }
+		internal List<Person> Szemelyek { get => szemelyek; set => szemelyek = value; }
+		internal List<Evidence> Bizonyitekok { get => bizonyitekok; set => bizonyitekok = value; }
+	}
+}
