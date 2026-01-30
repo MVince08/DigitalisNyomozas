27c85aca34d5e75e9e673a90b3ca569383f0d570
Vince <molnar.vince.bela@petrik.hu>
2026-01-30 10:52:33 +0100

    Kész CaseManager osztaly, de meg nem csinálja  a feladatat

---
+++ DigitalisNyomozas/CaseManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozas
+{
+	internal class CaseManager
+	{
+		List<Case> ugyek;
+
+		public CaseManager(List<Case> ugyek)
+		{
+			this.ugyek = ugyek;
+		}
+
+		internal List<Case> Ugyek { get => ugyek; set => ugyek = value; }
+	}
+}
