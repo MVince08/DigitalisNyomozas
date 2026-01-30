57fc034a3fd90b755567d0243c12bb4d73b5aa0b
Vince <molnar.vince.bela@petrik.hu>
2026-01-30 10:49:21 +0100

    Hopp elroltottam az előző commit ez :D

---
+++ DigitalisNyomozas/EvidenceManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozas
+{
+	internal class EvidenceManager
+	{
+		private List<Evidence> evidenceList;
+
+		public EvidenceManager(List<Evidence> evidenceList)
+		{
+			this.evidenceList = evidenceList;
+		}
+
+		internal List<Evidence> EvidenceList { get => evidenceList; set => evidenceList = value; }
+
+	}
+}
